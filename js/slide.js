70e0fc3e7720ed6bc972bc105dafb80f5332c91d
bruno.duarte <bruno.duarte@zaffari.com.br>
2024-10-02 00:18:05 -0300

    configuracoes e index dos slides

--- js/slide.js
+++ js/slide.js
@@ -42,9 +42,40 @@ export default class Slide {
         this.onEnd = this.onEnd.bind(this);
     }
 
+    //slides config
+    slidePosition(slide) {
+        //valor calculado para a img ficar no centro respeitando a margin
+        const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2; //offsetWidth - para pegar o tamannho do container
+        return -(slide.offsetLeft - margin); //tem que ser negativo para correr para esquerda
+    }
+
+    slidesConfig() {
+        this.slideArray = [...this.slide.children].map((element) => {
+            const position = this.slidePosition(element);
+            return { position, element };
+        }); 
+    }
+
+    slidesIndexNav(index) {
+        const last = this.slideArray.length - 1;
+        this.index = {
+            prev: index ? index - 1 : undefined,  //index for "0" retorna "false"
+            active: index,
+            next: index === last ? undefined : index + 1
+        }
+    }
+
+    changeSlide(index) {
+        const activeSlide = this.slideArray[index];
+        this.moveSlide(activeSlide.position);
+        this.slidesIndexNav(index);
+        this.dist.finalPosition = activeSlide.position;
+    }
+
     init() {
         this.bindEvents();
         this.addSlideEvents();
+        this.slidesConfig();
         return this;
     }
 }
